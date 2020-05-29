44cd653563166fc1d505bec736092cc92cb61929
Rita G. Nunes <ritagnunes@tecnico.ulisboa.pt>
2020-05-29 15:59:50 +0100

    included ADC estimation function

--- process_data.m
+++ process_data.m
@@ -1,15 +1,17 @@
 %Script for reconstructing the images, pre-process them 
-%and estimate Diffusion Tensor
+%and estimate Diffusion Tensor or simple ADC maps
 
-kname='kInVivo20sl1b60dirs';
-img_name='DWI_InVivo1b60dirs';
+kname='kPhantom3dirs5b3slices.mat';
+img_name='DWI_Pht5b3dirs';
+
+% In-vivo or phantom?
+invivo=0;
 
 %diffusion acquisition parameters
-Nb0s=1;
-Nb=1;
+Nb=5;%excluding non-DWI
 bmax=1000;
-Ndir=60;
-Nsl=1;
+Ndir=3;
+Nsl=3;
 
 %Partial Fourier factor and image spatial resolution
 PFourier=0.75;
@@ -18,52 +20,82 @@ Resy=2.5;
 %slice thickness including gaps if they exist
 Resz=2.5;%
 
+% model 'dti' or 'adc'
+model = 'adc'
+
 %include eddy current and motion correction pre-processing
 eddy=0;
+
+%for consistency with the FSL reference frame, 
+%we may need to multiply the y gradient component by -1
+fsl_flag=1;
 
 %%
 % https://www.mathworks.com/matlabcentral/fileexchange/8797-tools-for-nifti-and-analyze-image
 % MATLAB Central File Exchange
 addpath('nifti_tools')
+setenv('FSLOUTPUTTYPE','NIFTI')
 
 load(kname);
 
-dim_struct=struct('Nb0s',Nb0s,'Nb',Nb,'Ndir',Ndir,'Nsl',Nsl,'PFourier',PFourier);
+[bvals, bvecs, Nb0s] = generate_bvalsbvecs(bmax,Ndir,Nb,fsl_flag);
+save('bvals','bvals','-ASCII')
+save('bvecs','bvecs','-ASCII')
+
+dim_struct=struct('Nb',Nb,'Ndir',Ndir,'Nb0s',Nb0s,'Nsl',Nsl,'PFourier',PFourier);
 %recon sum-of-squares image including ghost correction
 im_sos = recon_imgs(k, dim_struct, 1);
 
 ni=make_nii(im_sos,[Resx Resy Resz]);
 save_nii(ni,strcat(img_name,'.nii'));
 
-[bvals, bvecs] = generate_bvalsbvecs(bmax,Ndir,Nb0s,Nb,1);
-save('bvals','bvals','-ASCII')
-save('bvecs','bvals','-ASCII')
+if invivo
+    %perform brain extraction
+    strCmd=sprintf('fslroi %s nonDWI 0 1',img_name);
+    [st,res]=system(strCmd);
+    strCmd='bet nonDWI nonDWI_brain -Z -m -f 0.4';
+    [st,res]=system(strCmd);
+else
+    %define masking threshold
+    midSl=round(Nsl/2);
+    midImg=im_sos(:,:,midSl,1);
+    thr = prctile(midImg(:),80);
+    mask=zeros(size(im_sos(:,:,:,1)));
+    mask(im_sos(:,:,:,1)>=thr)=1;    
+end
 
 if eddy
     %perform motion and eddy-current distortion correction
     disp('Performing eddy current distortion correction...')
-    strCmd=sprintf('dtifit -k %s -m mask.nii -o dti -b bvals -r bvecs',img_name);
+    generate_eddyfiles(dim_struct);
+    strCmd=sprintf('eddy_openmp --imain=%s --mask=nonDWI_brain_mask --bvals=bvals --bvecs=bvecs --out=%s_cor --acqp=acqp --index=index',img_name,img_name);
     [st,res]=system(strCmd);
 end
 
-%perform brain extraction
-if eddy
-    strCmd=sprintf('bet -k %s_cor %s_brain -Z -m',img_name,img_name);
-    [st,res]=system(strCmd);
-else    
-    strCmd=sprintf('bet -k %s %s_brain -Z -m',img_name,img_name);
-    [st,res]=system(strCmd);
+switch lower(model)
+    case 'dti'
+        %estimate Diffusion Tensor and derived maps
+        disp('estimating diffusion tensor...')
+        if eddy
+            strCmd=sprintf('dtifit -k %s_cor -m nonDWI_brain_mask -o %s_dti -b bvals -r bvecs',img_name,img_name);
+            [st,res]=system(strCmd);
+        else
+            strCmd=sprintf('dtifit -k %s -m nonDWI_brain_mask -o %s_cor_dti -b bvals -r bvecs',img_name,img_name);
+            [st,res]=system(strCmd);
+        end
+    case 'adc'
+         %estimate ADC maps
+        disp('estimating ADC map...')
+        if invivo            
+            tmp=load_nii('nonDWI_brain_mask.nii');
+            mask=tmp.img;
+        end
+        adc=estimate_adc(im_sos,mask, dim_struct, bmax);
+        ni=make_nii(adc,[Resx Resy Resz]);
+        save_nii(ni,strcat(img_name,'_adc.nii'));
+    otherwise
+        disp('model should be equal to either dti or adc')
 end
-
-%estimate Diffusion Tensor and derived maps
-if eddy
-    strCmd=sprintf('dtifit -k %s_cor -m %s_brain -o %s_dti -b bvals -r bvecs',img_name,img_name,img_name);
-    [st,res]=system(strCmd);
-else    
-    strCmd=sprintf('dtifit -k %s -m %s_brain -o %s_dti -b bvals -r bvecs',img_name,img_name,img_name);
-    [st,res]=system(strCmd);
-end
-
 
 
 
--- recon_imgs.m
+++ recon_imgs.m
@@ -1,6 +1,6 @@
 function im_sos = recon_imgs(k, dimpars ,gc)
 %im_sos = recon_imgs(k, dimpars )
-%k: k-space data should already have been be loaded into Matlab 
+%k: k-space data should already have been be loaded into Matlab
 %using a raw data reading function
 %compatible with the used MRI scanner's raw data structure
 %
@@ -16,16 +16,17 @@ function im_sos = recon_imgs(k, dimpars ,gc)
 % N - matrix size (assuming square field-of-view)
 % Nc - number of coil channels
 % where N and Nc are estimated from the data and
-% Nvol - number of volumes (Nb0 + Nb*Ǹdir)
-% Nimg - number of imgs = (1+Nvol) * Nsl
+% Nvol - number of volumes (1 + Nb0 + Nb*Ǹdir)
 % extra volume correponds to calibration data for EPI ghost-correction
+% Nimg - number of imgs = (Nvol-1) * Nsl
 %
 %gc: flag for ghost correction - On by default
 N = size(k,1);
 Nc = size(k,2);
 
-Nvol = dimpars.Nb0 + dimpars.Nb*dimpars.Ndir;
-Nimg = dimpars.Nsl*(Nvol+1);
+Nsl = dimpars.Nsl;
+Nvol = dimpars.Nb0s + dimpars.Nb*dimpars.Ndir;
+Nimg = Nsl*Nvol;
 PFourier = dimpars.PFourier;
 
 %gc: flag for ghost correction - On by default
@@ -33,96 +34,100 @@ if nargin<3
     gc=1;
 end
 
-if ~isequal(Nimg,size(k,3)/N)
-    disp('Inconsistent data size! Check provided dimensions.')
-end
-
 if or(PFourier < 0.6, PFourier >1)
     disp('The partial Fourier parameter has to been within the [0.6, 1.0] interval')
     return
 end
-    
+
+if ~isequal(Nimg+Nsl,size(k,3)/(N*PFourier))
+    disp('Inconsistent data size! Check provided dimensions.')
+    return
+end
+
+% extra volume (Nsl slices) correponds to calibration data for EPI ghost-correction
 if PFourier<1
     Ny=N*PFourier;
     pad=N-Ny;
-    k=reshape(k,[N Nc Ny Nimg]);
+    k=reshape(k,[N Nc Ny Nsl Nvol+1]);
 else
-    k=reshape(k,[N Nc N Nimg]);
+    k=reshape(k,[N Nc N Nsl Nvol+1]);
 end
 
 %calibration data is not required after reconstruction
-im=zeros([N Nc N Nimg]);
+im=zeros([N N Nc Nsl Nvol]);
 
-% reorder to obtain
-% N x N x Nc x Nimg
-im=permute(im,[1 3 2 4]);
-
-%read calibration lines first and reorder (N x N x Nc)
-kcalib=permute(k(:,:,:,1),[1 3 2]);
+%read calibration lines first and reorder (N x N x Nc x Nsl)
+kcalib=permute(k(:,:,:,:,1),[1 3 2 4]);
 
 %EPI re-ordering: mirror even k-space lines
-kcalib(:,2:2:end,:)=permute(k(end:-1:1,:,2:2:end,1),[1 3 2]);
+kcalib(:,2:2:end,:,:)=permute(k(end:-1:1,:,2:2:end,:,1),[1 3 2 4]);
 
 %basic ghost correction is carried out by subtracting
 %the phase of projection data (pcor)
-hybcalib=fftshift(fft(kcalib,[],1),1);
+hybcalib=fftshift(fft(ifftshift(kcalib,1),[],1),1);
 pcor=exp(-1i*angle(hybcalib));
 
-for ii=2:Nimg
-    for c=1:Nc
-        ktmp=squeeze(k(:,c,:,ii));
-        %EPI re-ordering: mirror even k-space lines
-        ktmp(:,2:2:end)=squeeze(k(end:-1:1,c,2:2:end,ii));
-        if gc
-            hybtmp=fftshift(fft(ifftshift(ktmp,1),[],1),1);
-            hybcor=hybtmp.*pcor(:,:,c);
-             %perform homodyne reconstruction
-            if PFourier<1
-                kcor=fftshift(ifft(ifftshift(hybcor,1),[],1),1);
-                % zero-fill
-                kn=zeros(N);
-                kn(:,pad+1:end)=kcor;
-                %keep only central portion for phase corrections
-                kn(:,end:-1:end-pad+1)=0;
-                %perform phase-correction
-                img=fftshift(fft2(ifftshift(kn)));
-                img=img.*exp(-1i*angle(img));
-                % back to k-space
-                kn=fftshift(ifft2(ifftshift(img)));
-                kn(:,1:pad)=0;
-                
-                % apply Hermitian symmetry to fill in unacquired data
-                kn(:,1:pad)=conj(kn(end:-1:1,end:-1:end-pad+1));
-                im(:,:,c,ii-1)=fftshift(fft2(ifftshift(kn)));
+for v=2:Nvol+1
+    for s=1:Nsl
+        for c=1:Nc
+            ktmp=squeeze(k(:,c,:,s,v));
+            %EPI re-ordering: mirror even k-space lines
+            ktmp(:,2:2:end)=squeeze(k(end:-1:1,c,2:2:end,s,v));
+            if gc
+                hybtmp=fftshift(fft(ifftshift(ktmp,1),[],1),1);
+                hybcor=hybtmp.*pcor(:,:,c,s);
+                %perform homodyne reconstruction
+                if PFourier<1
+                    kcor=fftshift(ifft(ifftshift(hybcor,1),[],1),1);
+                    % zero-fill
+                    kn=zeros(N);
+                    kn(:,pad+1:end)=kcor;
+                    %keep only central portion for phase corrections
+                    kn(:,end:-1:end-pad+1)=0;
+                    %perform phase-correction
+                    img=fftshift(fft2(ifftshift(kn)));
+                    img=img.*exp(-1i*angle(img));
+                    % back to k-space
+                    kn=fftshift(ifft2(ifftshift(img)));
+                    kn(:,1:pad)=0;
+                    
+                    % apply Hermitian symmetry to fill in unacquired data
+                    kn(:,1:pad)=conj(kn(end:-1:1,end:-1:end-pad+1));
+                    im(:,:,c,s,v-1)=fftshift(fft2(ifftshift(kn)));
+                else
+                    im(:,:,c,s,v-1)=fftshift(fft(ifftshift(hybcor,2),[],2),2);
+                end
             else
-                im(:,:,c,ii-1)=fftshift(fft(ifftshift(hybcor,2),[],2),2);
-            end
-        else
-            %perform homodyne reconstruction
-            if PFourier<1
-                % zero-fill
-                kn=zeros(N);
-                kn(:,pad+1:end)=ktmp;
-                %keep only central portion for phase corrections
-                kn(:,end:-1:end-pad+1)=0;
-                %perform phase-correction
-                img=fftshift(fft2(ifftshift(kn)));
-                img=img.*exp(-1i*angle(img));
-                % back to k-space
-                kn=fftshift(ifft2(ifftshift(img)));
-                kn(:,1:pad)=0;
-                
-                % apply Hermitian symmetry to fill in unacquired data
-                kn(:,1:pad)=conj(kn(end:-1:1,end:-1:end-pad+1));
-                im(:,:,c,ii-1)=fftshift(fft2(ifftshift(kn)));
-            else
-                im(:,:,c,ii-1)=fftshift(fft2(ifftshift(ktmp)));
+                %perform homodyne reconstruction
+                if PFourier<1
+                    % zero-fill
+                    kn=zeros(N);
+                    kn(:,pad+1:end)=ktmp;
+                    %keep only central portion for phase corrections
+                    kn(:,end:-1:end-pad+1)=0;
+                    %perform phase-correction
+                    img=fftshift(fft2(ifftshift(kn)));
+                    img=img.*exp(-1i*angle(img));
+                    % back to k-space
+                    kn=fftshift(ifft2(ifftshift(img)));
+                    kn(:,1:pad)=0;
+                    
+                    % apply Hermitian symmetry to fill in unacquired data
+                    kn(:,1:pad)=conj(kn(end:-1:1,end:-1:end-pad+1));
+                    im(:,:,c,s,v-1)=fftshift(fft2(ifftshift(kn)));
+                else
+                    im(:,:,c,s,v-1)=fftshift(fft2(ifftshift(ktmp)));
+                end
             end
         end
     end
 end
-    
-end
 %calculate sum-of-squares image, combining all coils
 im_sos=squeeze(sum(im.*conj(im),3)).^0.5;
+im_sos = reshape(im_sos,[N N Nsl Nvol]);
+
+%reorder slices
+im_sos = im_sos(:,:,end:-1:1,:);
+
+end
 

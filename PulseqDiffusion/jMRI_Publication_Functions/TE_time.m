a510506faa798a4ff0126c37d342644cc3cf797f
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2020-06-24 15:55:43 +0100

    Update TE_time.m

--- PulseqDiffusion/jMRI_Publication_Functions/TE_time.m
+++ PulseqDiffusion/jMRI_Publication_Functions/TE_time.m
@@ -1,16 +1,13 @@
-%% Read Me
-% 
-% by:
-% T.T. Fernandes, September 2019
-% LarSys - Instituto Superior T�cnico - Universidade de Lisboa
-%
-% name: TE_time
-% type: Function
-% description: Echo Time (TE) (s) for a Spiral sequence
-%
-
-%%
 function [TE_spiral] = TE_time(b_value,gmMax,gamma)
+% Echo Time (TE) (s) for a Spiral sequence
+% by T.T. Fernandes, September 2019 - LarSys - Instituto Superior T�cnico - Universidade de Lisboa
+% usage: TE_spiral = TE_time(b_value,gmMax,gamma)
+%
+% :parameters: b_value: b-value in s*mm^-2
+% :parameters: gmMax: Gradient maximum amplitude in T/m
+% :parameters: gamma: gyromagnetic constant in Hz/T
+%
+% :returns: TE_spiral: Longitudinal Steady-State signal
 
 TE_spiral = ((12 .* b_value) ./ ((gamma.*2*pi).^2 * gmMax.^2)).^(1/3);
 

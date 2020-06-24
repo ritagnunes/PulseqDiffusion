486368e9fc07fc49aa6fb94744224c53d4f84727
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2020-06-24 15:45:48 +0100

    Update Ernstangle_d.m

--- PulseqDiffusion/jMRI_Publication_Functions/Ernstangle_d.m
+++ PulseqDiffusion/jMRI_Publication_Functions/Ernstangle_d.m
@@ -1,16 +1,12 @@
-%% Read Me
-% 
-% by:
-% T.T. Fernandes, September 2019
-% LarSys - Instituto Superior T�cnico - Universidade de Lisboa
-%
-% name: Ernstangle_d
-% type: Function
-% description: It obtains the Ernst Angle Calculation (degrees)
-%
-
-%%
 function [ernstAngle] = Ernstangle_d(TRep,T1)
+% It obtains the Ernst Angle Calculation in degrees
+% by T.T. Fernandes, September 2019 - LarSys - Instituto Superior T�cnico - Universidade de Lisboa
+% usage:ernstAngle = Ernstangle_d(TRep,T1)
+%
+% :parameters: TRep: Time of readout in s
+% :parameters: T1: T1 time in seconds
+%
+% :returns: ernstAngle: ernst Angle in degrees
 
 ernstAngle = acosd( exp(-TRep./T1) );
 

9b2915cc54b35d9dba1ff78aa0bbcc86c35b3bb7
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2020-06-24 15:58:13 +0100

    Update aux_delta.m

--- PulseqDiffusion/jMRI_Publication_Functions/aux_delta.m
+++ PulseqDiffusion/jMRI_Publication_Functions/aux_delta.m
@@ -11,6 +11,16 @@
 
 %%
 function [aDelta] = aux_delta(b_value,gmMax,part_Tend,gamma)
+%  Auxiliar function for calculation of delta for an EPI sequence (see 'delta.m' function & 'Calc_delta_EPI.pdf')
+% by T.T. Fernandes, September 2019 - LarSys - Instituto Superior T�cnico - Universidade de Lisboa
+% usage: aDelta = aux_delta(b_value,gmMax,part_Tend,gamma)
+%
+% :parameters: b_value: b-value in s*mm^-2
+% :parameters: gmMax: Gradient maximum amplitude in T/m
+% :parameters: part_Tend: Sequence readout time until k_space center in s
+% :parameters: gamma: gyromagnetic constant in Hz/T
+%
+% :returns: aDelta: auxiliar delta
 
 aDelta = (  6*b_value*(gamma.*2*pi*gmMax).^4  - (gamma.*2*pi*gmMax).^6.*part_Tend.^3  +  2*sqrt(3)*sqrt(  3*b_value^2*(gamma.*2*pi*gmMax).^8 - b_value*(gamma.*2*pi*gmMax).^10.*part_Tend.^3  )  ).^(1/3);
 

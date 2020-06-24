bcf72896c88c301dd7382239ef2b4976f30d4b12
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2020-06-24 16:00:27 +0100

    Update delta.m

--- PulseqDiffusion/jMRI_Publication_Functions/delta.m
+++ PulseqDiffusion/jMRI_Publication_Functions/delta.m
@@ -1,16 +1,14 @@
-%% Read Me
-% 
-% by:
-% T.T. Fernandes, September 2019
-% LarSys - Instituto Superior T�cnico - Universidade de Lisboa
-%
-% name: delta
-% type: Function
-% description: delta for TE in (s) for an EPI sequence. It follows the demonstration provided in the 'Calc_delta_EPI.pdf' GITHUB folder 
-%
-
-%%
 function [DELTA] = delta(gmMax,part_Tend,aux_delta,gamma)
+% Delta for TE in (s) for an EPI sequence. It follows the demonstration provided in the 'Calc_delta_EPI.pdf' GITHUB folder .
+% by T.T. Fernandes, September 2019 - LarSys - Instituto Superior T�cnico - Universidade de Lisboa
+% usage: DELTA = delta(gmMax,part_Tend,aux_delta,gamma)
+%
+% :parameters: gmMax: Gradient maximum amplitude in T/m
+% :parameters: part_Tend: Sequence readout time until k_space center in s
+% :parameters: aux_delta: see 'aux_delta.m' function
+% :parameters: gamma: gyromagnetic constant in Hz/T
+%
+% :returns: DELTA: Time of diffusion gradient in s
 
 DELTA = (-part_Tend)/2  - ((1-1*i*sqrt(3))*(gamma.*2*pi*gmMax.*part_Tend).^2) ./ (4*aux_delta)  -  ((1-1*i*sqrt(3))*aux_delta) ./ (4*(gamma.*2*pi*gmMax).^2);
 

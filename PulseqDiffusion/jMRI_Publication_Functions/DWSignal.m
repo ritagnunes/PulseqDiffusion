2b12f87fbef97f96074bbbeeedc324d7c8ded6f8
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2020-06-25 16:36:01 +0100

    Update DWSignal.m

--- PulseqDiffusion/jMRI_Publication_Functions/DWSignal.m
+++ PulseqDiffusion/jMRI_Publication_Functions/DWSignal.m
@@ -1,6 +1,6 @@
 function [DWs] = DWSignal(b_value,ADC)
 % Diffusion signal calculation
-% by T.T. Fernandes, September 2019 - LarSys - Instituto Superior T�cnico - Universidade de Lisboa
+% by T.T. Fernandes, September 2019 - LarSys - Instituto Superior Tecnico - Universidade de Lisboa
 % usage:DWs = DWSignal(b_value,ADC)
 %
 % :parameters: b_value: value associated with the test in s*mm^-2

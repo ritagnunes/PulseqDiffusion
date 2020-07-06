dbf37e5e8de0c48831f75288dede6f5200f2e48e
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2020-07-06 10:18:35 +0100

    Update TE_timeTest.m

--- PulseqDiffusion/jMRI_Publication_Functions/TE_timeTest.m
+++ PulseqDiffusion/jMRI_Publication_Functions/TE_timeTest.m
@@ -1,11 +1,12 @@
 %% Test Example TE_time for the following parameters: b_value=400 s*mm^-2, gmMax=0.016 T/m, gamma=42.577e6 Hz/T
-addpath(genpath('../SNR_CNR_Study'));
+clear all
+
+addpath(genpath('./SNR_CNR_Study'));
 
 b_value = 400*1e6;
 gmMax   = 0.016;
 gamma   = 42.577e6;
-
-TEtime_pred = load('../Example_Data/TE_time_TestResults.mat'); % in ms
+TEtime_pred = load('./Example_Data/TE_time_TestResults.mat'); % in ms
 TEtime_act  = TE_time(b_value,gmMax,gamma); % in ms
 assertValue(TEtime_act,TEtime_pred.TE_VectorTest(1))
 clear b_value gmMax gamma
@@ -17,7 +18,7 @@ gamma   = 42.577e6;
 
 % TEtime_pred = ; % in ms
 TEtime_act  = TE_time(b_value,gmMax,gamma); % in ms
-assertValue(TEtime_act,TEtime_pred.TE_VectorTest(1))
+assertValue(TEtime_act,TEtime_pred.TE_VectorTest(2))
 clear b_value gmMax gamma
 
 %% Test Example TE_time for the following parameters: b_value=800 s*mm^-2,gmMax=0.22 T/m,gamma=42.577e6 Hz/T
@@ -27,10 +28,13 @@ gamma   = 42.577e6;
 
 % TEtime_pred = ; % in ms
 TEtime_act  = TE_time(b_value,gmMax,gamma); % in ms
-assertValue(TEtime_act,TEtime_pred.TE_VectorTest(1))
+assertValue(TEtime_act,TEtime_pred.TE_VectorTest(3))
 
 clear b_value gmMax gamma
 
+%%
+
+fprintf('All test for TE_timeTest run!')    
 %%
 function assertValue(actVal,expVal)
 % Helper function to assert equality

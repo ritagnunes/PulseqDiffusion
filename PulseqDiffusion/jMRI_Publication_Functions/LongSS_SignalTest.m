9c22d8f34595632423c56382376b639c88d3568a
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2020-07-06 15:51:56 +0100

    add delta & LongSS_Signal tests

---
+++ PulseqDiffusion/jMRI_Publication_Functions/LongSS_SignalTest.m
@@ -0,0 +1,47 @@
+%% Test Example delta for the following parameters: FlipAngle=90 degrees, TRep=17.3011 s, TE=0.1739 s, T1=0.9982 s, T2=0.0727 s
+clear all
+
+addpath(genpath('./SNR_CNR_Study'));
+
+FlipAngle = 90;
+TRep      = 17.3011;
+TE        = 0.1739;
+T1        = 0.9982; 
+T2        = 0.0727; 
+LongSS_signal_pred = load('./Example_Data/LongSS_Signal_TestResults.mat'); % in s
+LongSS_signal_act  = LongSS_Signal(FlipAngle,TRep,TE,T1,T1);
+assertValue(LongSS_signal_act,LongSS_signal_pred.LongSSsignal_VectorTest(1))
+clear FlipAngle TRep TE T1 T2
+
+%% Test Example delta for the following parameters: FlipAngle=81 degrees, TRep=3.2833 s, TE=0.0143 s, T1=0.1004 s, T2=0.0639 s
+
+FlipAngle = 81;
+TRep      = 3.2833;
+TE        = 0.0143;
+T1        = 0.1004; 
+T2        = 0.0639; 
+LongSS_signal_act  = LongSS_Signal(FlipAngle,TRep,TE,T1,T1);
+assertValue(LongSS_signal_act,LongSS_signal_pred.LongSSsignal_VectorTest(2))
+clear FlipAngle TRep TE T1 T2
+
+
+%% Test Example delta for the following parameters: FlipAngle=90 degrees, TRep=2.9760 s, TE=0.0132 s, T1=0.1004 s, T2=0.0639 s
+
+FlipAngle = 90;
+TRep      = 2.9760;
+TE        = 0.0132;
+T1        = 0.1004; 
+T2        = 0.0639; 
+LongSS_signal_act  = LongSS_Signal(FlipAngle,TRep,TE,T1,T1);
+assertValue(LongSS_signal_act,LongSS_signal_pred.LongSSsignal_VectorTest(3))
+clear FlipAngle TRep TE T1 T2
+
+fprintf('All test for LongSS_SignalTest run!')    
+%%
+function assertValue(actVal,expVal)
+% Helper function to assert equality
+% Takes two images and compares them 
+
+tf = isequal(actVal,expVal);
+assert(tf, 'TE values do not Match!');
+end
--- PulseqDiffusion/jMRI_Publication_Functions/deltaTest.m
+++ PulseqDiffusion/jMRI_Publication_Functions/deltaTest.m
@@ -1,35 +1,36 @@
-%% Test Example delta for the following parameters: aux_delta=, gmMax=0.030 T/m, part_Tend=0.0458 s, gamma=42.577e6 Hz/T
+%% Test Example delta for the following parameters: aux_delta=aux_delta_TestResults(1), gmMax=0.030 T/m, part_Tend=0.0458 s, gamma=42.577e6 Hz/T
 clear all
 
 addpath(genpath('./SNR_CNR_Study'));
 
-aux_delta = 10*1e6;
+aDelta = load('./Example_Data/aux_delta_TestResults.mat'); % in s
+aux_delta = aDelta.aDelta_VectorTest(1);
 gmMax     = 0.030;
 part_Tend = 0.0458;
 gamma     = 42.577e6; 
-Delta_pred = load('./Example_Data/delta_TestResults.mat');
+Delta_pred = load('./Example_Data/delta_TestResults.mat'); % in s
 Delta_act  = delta(gmMax,part_Tend,aux_delta,gamma);
 assertValue(Delta_act,Delta_pred.Delta_VectorTest(1))
 clear b_value gmMax part_Tend gamma
 
-%% Test Example aux_delta for the following parameters: b_value=1000 s*mm^-2, gmMax=0.014 T/m, part_Tend=0.0458 s, gamma=42.577e6 Hz/T
+%% Test Example aux_delta for the following parameters: aux_delta=aux_delta_TestResults(2), gmMax=0.014 T/m, part_Tend=0.0458 s, gamma=42.577e6 Hz/T
 
-aux_delta = 10*1e6;
+aux_delta = aDelta.aDelta_VectorTest(2);
 gmMax     = 0.014;
 part_Tend = 0.0458;
 gamma     = 42.577e6; 
 Delta_act  = delta(gmMax,part_Tend,aux_delta,gamma);
-assertValue(Delta_act,Delta_pred.Delta_VectorTest(1))
+assertValue(Delta_act,Delta_pred.Delta_VectorTest(2))
 clear b_value gmMax part_Tend gamma
 
-%% Test Example aux_delta for the following parameters: b_value=200 s*mm^-2, gmMax=0.020 T/m, part_Tend=0.0458 s, gamma=42.577e6 Hz/T
+%% Test Example aux_delta for the following parameters: aux_delta=aux_delta_TestResults(3), gmMax=0.020 T/m, part_Tend=0.0458 s, gamma=42.577e6 Hz/T
 
-aux_delta = 10*1e6;
+aux_delta = aDelta.aDelta_VectorTest(3);
 gmMax     = 0.020;
 part_Tend = 0.0458;
 gamma     = 42.577e6; 
 Delta_act  = delta(gmMax,part_Tend,aux_delta,gamma);
-assertValue(Delta_act,Delta_pred.Delta_VectorTest(1))
+assertValue(Delta_act,Delta_pred.Delta_VectorTest(3))
 clear b_value gmMax part_Tend gamma
 
 fprintf('All test for deltaTest run!')    

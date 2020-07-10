39b374bb50fd69fc0e134f00bd2968187ad44b26
opennog <opennog@gmail.com>
2020-07-10 08:39:28 +0200

    Add files via upload
    updated utests

---
+++ PulseqDiffusion/test/utest_diff_funcs_calc_exact_bval.py
@@ -0,0 +1,46 @@
+import pickle
+import sys
+import unittest
+from pathlib import Path
+
+from pypulseq import opts
+from pypulseq.Sequence import sequence
+
+if __name__ == '__main__':
+    path = Path(__file__).absolute().parent.parent.parent
+    sys.path.insert(0, str(path))
+
+import PulseqDiffusion.diff_funcs as difunc
+
+sys.modules['sequence'] = sequence
+sys.modules['opts'] = opts
+
+
+class TestCalexactbval(unittest.TestCase):
+    def test_calexacetbval1(self):
+        path = Path(__file__).parent.parent / 'utest_sample_data' / 'calc_exact_bval1.pkl'
+        with open(path, 'rb') as f:
+            waveform, INV, short_f, seq = pickle.load(f)
+
+        result = difunc.calc_exact_bval(waveform, INV, short_f, seq)
+        self.assertEqual(result, 327.19201047729877)
+
+    def test_calexacetbval2(self):
+        path = Path(__file__).parent.parent / 'utest_sample_data' / 'calc_exact_bval2.pkl'
+        with open(path, 'rb') as f:
+            waveform, INV, short_f, seq = pickle.load(f)
+
+        result = difunc.calc_exact_bval(waveform, INV, short_f, seq)
+        self.assertEqual(result, 498.775665378322)
+
+    def test_calexacetbval3(self):
+        path = Path(__file__).parent.parent / 'utest_sample_data' / 'calc_exact_bval3.pkl'
+        with open(path, 'rb') as f:
+            waveform, INV, short_f, seq = pickle.load(f)
+
+        result = difunc.calc_exact_bval(waveform, INV, short_f, seq)
+        self.assertEqual(result, 749.6658744624788)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ PulseqDiffusion/test/utest_diff_funcs_opt_TE_bv_TRSE.py
@@ -0,0 +1,55 @@
+import pickle
+import sys
+import unittest
+from pathlib import Path
+
+from pypulseq import opts
+from pypulseq.Sequence import sequence
+
+if __name__ == '__main__':
+    path = Path(__file__).absolute().parent.parent.parent
+    sys.path.insert(0, str(path))
+
+import PulseqDiffusion.diff_funcs as difunc
+
+sys.modules['sequence'] = sequence
+sys.modules['opts'] = opts
+
+
+class TestOptTEbvTRSE(unittest.TestCase):
+    def test_optTEbvTRSE1(self):
+        path = Path(__file__).parent.parent / 'utest_sample_data' / 'opt_TE_bv_TRSE1.pkl'
+        with open(path, 'rb') as f:
+            bvalue_Dict, grads_times_Dict, seq_sys_Dict, result_tmp = pickle.load(f)
+
+        result = difunc.opt_TE_bv_TRSE(bvalue_Dict, grads_times_Dict, seq_sys_Dict)
+        self.assertEqual(result[0:2], result_tmp[0:2])
+        self.assertTrue(self, all(result[2] == result_tmp[2]))
+        self.assertEqual(result[3:], result_tmp[3:])
+        print('------End test 1------')
+
+    def test_optTEbvTRSE2(self):
+        path = Path(__file__).parent.parent / 'utest_sample_data' / 'opt_TE_bv_TRSE2.pkl'
+        with open(path, 'rb') as f:
+            bvalue_Dict, grads_times_Dict, seq_sys_Dict, result_tmp = pickle.load(f)
+
+        result = difunc.opt_TE_bv_TRSE(bvalue_Dict, grads_times_Dict, seq_sys_Dict)
+        self.assertEqual(result[0:2], result_tmp[0:2])
+        self.assertTrue(self, all(result[2] == result_tmp[2]))
+        self.assertEqual(result[3:], result_tmp[3:])
+        print('------End test 2------')
+
+    def test_optTEbvTRSE3(self):
+        path = Path(__file__).parent.parent / 'utest_sample_data' / 'opt_TE_bv_TRSE3.pkl'
+        with open(path, 'rb') as f:
+            bvalue_Dict, grads_times_Dict, seq_sys_Dict, result_tmp = pickle.load(f)
+
+        result = difunc.opt_TE_bv_TRSE(bvalue_Dict, grads_times_Dict, seq_sys_Dict)
+        self.assertEqual(result[0:2], result_tmp[0:2])
+        self.assertTrue(self, all(result[2] == result_tmp[2]))
+        self.assertEqual(result[3:], result_tmp[3:])
+        print('------End test 3------')
+
+
+if __name__ == '__main__':
+    unittest.main()

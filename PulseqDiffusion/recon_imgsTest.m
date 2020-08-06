f625e8f47e85f5bdc3eb7fd542b4db31f9f50d60
ritagnunes <36857476+ritagnunes@users.noreply.github.com>
2020-08-06 15:02:39 +0100

    Update recon_imgsTest.m
    moved assertValue function to separate file

--- PulseqDiffusion/recon_imgsTest.m
+++ PulseqDiffusion/recon_imgsTest.m
@@ -18,11 +18,3 @@ tmp=load('../Example_Data/ImgPhantom3dirs5b3slices.mat');
 impred=tmp.im_sos;
 imact=recon_imgs(t.k, t.dim_struct);
 assertValue(imact,impred)
-
-function assertValue(actVal,expVal)
-% Helper function to assert equality
-% Takes two images and compares them 
-
-tf = isequal(actVal,expVal);
-assert(tf, 'Recon images do not match!');
-end
